dcd9a71e9b7a9d17bc3d29a636cfdc763b19e5a6
Kory Kessel <kokessel@gmail.com>
2021-07-11 19:09:47 -0400

    Update call to action with icons.

--- js/main.ts
+++ js/main.ts
@@ -1,5 +1,12 @@
 import "../css/main.less";
 
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope"
+
+library.add(faLinkedin, faEnvelope);
+dom.watch();
+
 const dataFadeAttribute = "data-fade";
 
 const getFade = (div: Element) => parseInt(div.getAttribute(dataFadeAttribute));
